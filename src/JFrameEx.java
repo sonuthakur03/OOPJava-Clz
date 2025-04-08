75333ce86da0adc3e95ecff0ea9a27562f385813
Sonu Thakur <136282638+sonuthakur03@users.noreply.github.com>
2025-04-08 09:37:36 +0545

    add comments

--- src/JFrameEx.java
+++ src/JFrameEx.java
@@ -3,16 +3,22 @@ import javax.swing.JLabel;
 
 public class JFrameEx {
     public static void main(String[] args) {
+//        new jframe created
         JFrame frame = new JFrame("My first jframe");
 
+//        new jlabel created
         JLabel label = new JLabel("Bca third semester group");
 
+//        adding label to frame
         frame.add(label);
 
+//        setting size of frame
         frame.setSize(500, 500);
 
+//        closing opr
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+//        visibility
         frame.setVisible(true);
     }
 }
